741107474ce2bd9b431a7c7995bc59db820ed1d6
Ismail Abbasi <ismail.official295@gmail.com>
2026-04-14 01:34:59 +0500

    feat: initialize socket.io server with basic message broadcasting functionality

--- server/server.ts
+++ server/server.ts
@@ -1,13 +1,11 @@
-import { Server,Socket } from "socket.io";
-const io = new Server(3000); // Running on port 3000
+import { Server, Socket } from "socket.io";
+const io = new Server(3000);
 
 io.on("connection", (socket) => {
   console.log("A user connected:", socket.id);
 
-  // Listen for a custom event
   socket.on("message", (data) => {
     console.log("Received:", data);
-    // Broadcast to all connected clients
     io.emit("broadcast", data);
   });
 });
